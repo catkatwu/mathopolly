79cd0876f7cf60572bbd6577376ce2109ea67b21
catkatwuacc <99305507+catkatwuacc@users.noreply.github.com>
2022-02-28 11:28:41 +0800

    Add files via upload

---
+++ asset/index.js
@@ -0,0 +1,80 @@
+document.write(
+
+'<body>',
+
+    '<div class="hero-black">',
+
+        '<div class="big-hero importbg center">',
+
+            '<div class="heading float white-text">',
+                '<p class="caps small-m">',
+                    'Project',
+                '</p>',
+            '</div>',
+
+            '<p class="mid-text small-m">',
+                'Mathopolly', '</p>',
+
+                '<p class="caps small-text">',
+                'Mechanical Accelarator Throught Outer Space with Polly Gear Engine Design',
+            '</p>',
+        '</div>',
+
+        '<div class="small-hero center white-text">',
+
+            '<p class="mid-text">','Project Mathopolly','</p>',
+
+            '<textarea name=""  class="white-border white-text mid-mt transparent small-text" disabled id="" cols="50" rows="15">',
+
+'    What is it?',
+'',
+'    It is a Airship Project that is Made ',
+'     from Metal ',
+'',
+'     and other strong material',
+'',
+'    It is a Concept for Now', 
+'',
+'    2.27.2022 (Asian Date)',
+'    2.26.2022 (American Date)',
+'',
+'    Design by Laucsap',
+'',
+            '</textarea>',
+
+            '<div class="item big-m">',
+                '<hr class="big-hr white small-m">',
+                '<hr class="big-hr link small-m">',
+                '<hr class="big-hr warning small-m">',
+                '<hr class="big-hr danger small-m">',
+            '</div>',
+            '<p class="mid-text">','Intentions','</p>',
+'',
+            '<textarea name=""  class="white-border white-text mid-mt transparent small-text" disabled id="" cols="50" rows="9">',
+' ',
+'    Space Spherical Ship',
+' ',
+'    001 Avoid Humans from Dying',
+    'in Earth 139121127260127',
+' ',
+'    002 Release Humans from',
+'    WW3-4',
+            '</textarea>',
+        '</div>',
+
+        '<div class="small-p center">',
+
+            '<img id="map_s" src="asset/space.png" alt="" class="white-border big-square">',
+            '<p class="white">','Concept of Shaping','</p>',
+        '</div>',
+
+        '<div class="small-p center">',
+
+            '<img id="map_l" src="asset/space.png" alt="" class="white-border big-square">',
+            '<p class="white">','Concept Look','</p>',
+        '</div>',
+    '</div>',
+
+'</body>',
+'</html>'
+)
